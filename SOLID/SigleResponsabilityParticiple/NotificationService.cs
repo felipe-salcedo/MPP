2573b0b52dcbc0753b856bb71efafdae35226852
felipe-salcedo <felipesalcedo,ramalho@gmail.com>
2025-02-18 16:03:51 -0300

    SRP

---
+++ SOLID/SigleResponsabilityParticiple/NotificationService.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace SigleResponsabilityParticiple
+{
+    public class NotificationService
+    {
+        public void NotifyClient()
+        {
+            // Lógica para notificar o cliente via e-mail ou SMS
+            Console.WriteLine("Notificação enviada ao cliente.");
+        }
+    }
+}
