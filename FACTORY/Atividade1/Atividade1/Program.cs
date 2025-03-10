5eb3d77c20b930f37c62f69599b4e0a287bb7209
FELIPE SALCEDO RAMALHO <0031432412008@lti.fatecso.edu.br>
2025-03-10 16:02:57 -0300

    atividades 1 e 2 de fatcory

---
+++ FACTORY/Atividade1/Atividade1/Program.cs
@@ -0,0 +1,59 @@
+﻿interface IVehicle
+{
+    void StartRoute();
+
+    void GetCargo();
+}
+
+class Car : IVehicle
+{
+    public void StartRoute()
+    {
+        Console.WriteLine("Carro pronto para trasportar passageiros");
+    }
+
+    public void GetCargo()
+    {
+        Console.WriteLine("Carro pronto para pegar os passageiros");
+    }
+}
+
+class Moto : IVehicle
+{
+    public void StartRoute()
+    {
+        Console.WriteLine("Moto pronto para trasportar produtos");
+    }
+
+    public void GetCargo()
+    {
+        Console.WriteLine("Moto pronto para pegar os produtos");
+    }
+}
+
+abstract class Transport
+{
+    public void StartTransport()
+    {
+        IVehicle vehicle = CreateTransport();
+        vehicle.StartRoute();
+    }
+
+    protected abstract IVehicle CreateTransport();
+}
+
+class CarTransport : Transport
+{
+    protected override IVehicle CreateTransport()
+    {
+        return new Car();
+    }
+}
+
+class MotoTransport : Transport
+{
+    protected override IVehicle CreateTransport()
+    {
+        return new Moto();
+    }
+}
---
+++ FACTORY/Atividade2/Atividade2/Program.cs
@@ -0,0 +1,80 @@
+﻿interface IVehicle
+{
+    void StartRoute();
+
+    void GetCargo();
+}
+
+class Car : IVehicle
+{
+    public void StartRoute()
+    {
+        Console.WriteLine("Carro pronto para trasportar passageiros");
+    }
+
+    public void GetCargo()
+    {
+        Console.WriteLine("Carro pronto para pegar os passageiros");
+    }
+}
+
+class Motorcycle : IVehicle
+{
+    public void StartRoute()
+    {
+        Console.WriteLine("Moto pronto para trasportar produtos");
+    }
+
+    public void GetCargo()
+    {
+        Console.WriteLine("Moto pronto para pegar os produtos");
+    }
+}
+
+class Bike : IVehicle
+{
+    public void StartRoute()
+    {
+        Console.WriteLine("Bicicleta pronta para trasportar o ciclista");
+    }
+
+    public void GetCargo()
+    {
+        Console.WriteLine("Bicicleta pronta para ser conduida pelo ciclista");
+    }
+}
+
+abstract class Transport
+{
+    public void StartTransport()
+    {
+        IVehicle vehicle = CreateTransport();
+        vehicle.StartRoute();
+    }
+
+    protected abstract IVehicle CreateTransport();
+}
+
+class CarTransport : Transport
+{
+    protected override IVehicle CreateTransport()
+    {
+        return new Car();
+    }
+}
+
+class MotorcycleTransport : Transport
+{
+    protected override IVehicle CreateTransport()
+    {
+        return new Motorcycle();
+    }
+}
+
+class BikeTransport : Transport
+{
+    protected override IVehicle CreateTransport()
+    {
+        return new Bike();
+    }
+}
