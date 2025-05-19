5549a7a07d8fe3e003fb8b53f9675ca83585c7b3
FELIPE SALCEDO RAMALHO <0031432412008@lti.fatecso.edu.br>
2025-05-19 15:58:57 -0300

    mostrando saldos no terminal

--- FACADE/AtividadeFacade/Program.cs
+++ FACADE/AtividadeFacade/Program.cs
@@ -13,6 +13,7 @@ class Program
         fachada.Deposito(1001, 200.0);
         fachada.Saque(1001, 100.0);
 
+
         // b) Criar usuário 2
         Usuario u2 = new Usuario(1002, "Carlos");
         u2.Saldo = 1000.0;
@@ -20,7 +21,13 @@ class Program
         fachada.Deposito(1002, 300.0);
         fachada.Saque(1002, 1200.0); // saque maior que o saldo, deve avisar
 
+
         // c) Saque com matrícula inexistente
         fachada.Saque(9999, 100.0);
+
+        Console.WriteLine($"Saldo final de {u1.Nome} (matrícula {u1.Matricula}): R$ {u1.Saldo:F2}");
+
+        Console.WriteLine($"Saldo final de {u2.Nome} (matrícula {u2.Matricula}): R$ {u2.Saldo:F2}");
     }
 }
+
